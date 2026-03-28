7a87613ae57540296ad976b0f79104d432ac791f
SahilPavaskar <sahilpavaskar81@gmail.com>
2026-03-27 19:26:22 -0700

    Add SNS topic and email subscription

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.37"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sns_topic" "incident_alerts" {
+  name = "guardduty-ir-alerts"
+}
+
+resource "aws_sns_topic_subscription" "email_alerts" {
+  topic_arn = aws_sns_topic.incident_alerts.arn
+  protocol  = "email"
+  endpoint  = "sahilpavaskar81@gmail.com"
+}
+
+output "sns_topic_arn" {
+  value = aws_sns_topic.incident_alerts.arn
+}
